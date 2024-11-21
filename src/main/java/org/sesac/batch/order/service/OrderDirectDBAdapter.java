2a789ffb13772df5f178d0ef8a116950a3152c1b
laigasus <okjaeook98@gmail.com>
2024-11-21 21:35:05 +0900

    feat: batch JdbcTemplate 작성
    `+` 주문 상태 없데이트
    `+` 비활성 주문 삭제

---
+++ src/main/java/org/sesac/batch/order/service/OrderDirectDBAdapter.java
@@ -0,0 +1,30 @@
+package org.sesac.batch.order.service;
+
+import org.sesac.batch.order.constant.OrderState;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderDirectDBAdapter {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int updateOrderState(OrderState before, OrderState after, int week) {
+        String sql = "UPDATE `order` SET order_state= ? " +
+                "WHERE order_state = ? " +
+                "AND order_date < DATE_SUB(NOW(), INTERVAL ? WEEK)";
+
+        return jdbcTemplate.update(sql, after.name(), before.name(), week);
+    }
+
+    public int deleteOrder(OrderState desired, int week) {
+        System.out.println("deleteOrder");
+        String sql = "DELETE FROM `order` " +
+                "WHERE order_state = ? " +
+                "AND order_date < DATE_SUB(NOW(), INTERVAL ? WEEK)";
+
+        return jdbcTemplate.update(sql, desired.name(), week);
+    }
+}
