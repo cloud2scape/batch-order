af3dcf5c0f6628a9e8f09be1edf9ac88f0ba765f
laigasus <okjaeook98@gmail.com>
2024-11-21 21:23:27 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/org/sesac/batch/order/BatchOrderApplication.java
@@ -0,0 +1,13 @@
+package org.sesac.batch.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchOrderApplication.class, args);
+    }
+
+}
